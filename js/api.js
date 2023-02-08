e8119bdada7e7f6f22ae3a24a9f47cf95bd9eac9
Bhargav gunreddy <bhargav.gunreddy@procore.com>
2023-02-08 18:41:20 -0500

    Update index.html

--- js/api.js
+++ js/api.js
@@ -1,3 +1,4 @@
+// Unable to import this in main file
 var getRobots = async function () {
   return fetch('https://60c8ed887dafc90017ffbd56.mockapi.io/robots')
     .then(function (data) {
--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 //
-// Place any custom JS here
-//
+// Function to make fetch call to get list of robots
+// unable to import this from api.js running into errors so adding it here
 var getRobots = async function () {
   return fetch('https://60c8ed887dafc90017ffbd56.mockapi.io/robots')
     .then(function (data) {
@@ -8,26 +8,31 @@ var getRobots = async function () {
     })
     .then(function (data) {
       return data;
+    })
+    .catch(function (error) {
+      console.log(error);
+      return [];
     });
 };
 
 // Fetch data
 
 var robotsListFromAPI = await getRobots();
+// keep track of search value
 var searchId = '';
 
 // Add data to the table
-
-console.log(robotsListFromAPI);
+// Loop through data and add rows to the table
 
 var buildTable = function (robotsList) {
   var tableElement = document.getElementById('data');
+  var robotsCount = document.getElementById('robots-count');
+  robotsCount.innerText = robotsList.length;
   var tableRows = '';
   if (robotsList === null || robotsList.length === 0) {
     tableRows = '<tr colspan="3"> No Rows to display</tr>';
   } else {
     robotsList.forEach((robot) => {
-      // console.log(robot);
       tableRows += '<tr>';
       tableRows +=
         '<td class="table-columns" name=" id">' + robot.robotId + '</td>';
@@ -41,11 +46,14 @@ var buildTable = function (robotsList) {
   }
   tableElement.innerHTML = tableRows;
 };
+
+// Populate the grid with filtered results
 var filterTable = function (robotsListFromAPI) {
   // buildTable
   buildTable(robotsListFromAPI);
 };
 
+// When search button is clicked
 var searchFunction = function (event) {
   event.preventDefault();
   // Get search id
@@ -60,17 +68,19 @@ var searchFunction = function (event) {
   filterTable(filteredList);
 };
 
+// To store search value
 var updateSearchValue = function (event) {
   searchId = event.target.value;
 };
 
+// To sort the table in ascending order
 var sortTable = function (event) {
-  console.log(event);
   let columnHeaderValue = event.target.getAttribute('name');
   robotsListFromAPI.sort((a, b) => a[columnHeaderValue] - b[columnHeaderValue]);
   filterTable(robotsListFromAPI);
 };
 
+// Attach event listeners
 document
   .getElementById('search-field')
   .addEventListener('input', updateSearchValue);
@@ -82,5 +92,5 @@ for (let header of headers) {
   header.addEventListener('click', sortTable);
 }
 
+// Render the table with data
 filterTable(robotsListFromAPI);
-// alert(robotsList);
